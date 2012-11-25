0fa220237949a7f092a69bf01ee89958545e51eb
iron9light <iron9light@gmail.com>
2012-11-25 23:23:22 +0800

    Add MacroJsonContextParser.Buffer

--- src/main/scala/net/liftweb/json/interpolation/MacroJsonContextParser.scala
+++ src/main/scala/net/liftweb/json/interpolation/MacroJsonContextParser.scala
@@ -18,12 +18,77 @@ package net.liftweb.json
 package interpolation
 
 import reflect.macros.Context
+import annotation.tailrec
 
 abstract class MacroJsonContextParser {
   val context: Context
+
   import context.universe._
+
+  private val EOF = (-1).asInstanceOf[Char]
+
   def parse(parts: Seq[Tree], args: Seq[Tree]): context.Expr[JValue] = {
     // todo
     context.literalNull
   }
+
+  private[interpolation] class Buffer(parts: Seq[Tree], args: Seq[Tree]) {
+    private[this] val partsIter = parts.iterator
+    private[this] val argsIter = parts.iterator
+
+    private[this] var current: Option[String] = None
+    private[this] var offset = 0
+    private[this] var start = context.enclosingPosition.startOrPoint
+    private[this] var isPart = true
+
+    {
+      if (partsIter.hasNext) {
+        val tree = partsIter.next()
+        val Literal(Constant(s: String)) = tree
+        current = Some(s)
+        start = tree.pos.startOrPoint
+      }
+    }
+
+    var eofIsFailure = false
+
+    def point: Int = start + offset
+
+    @tailrec
+    final def next: Either[Char, Tree] = {
+      if (isPart) {
+        current match {
+          case None =>
+            if (eofIsFailure) {
+              context.abort(context.enclosingPosition.withPoint(point), "unexpected eof")
+            } else {
+              Left(EOF)
+            }
+          case Some(s) if s.length <= offset =>
+            isPart = false
+            next
+          case Some(s) =>
+            val c = s(offset)
+            offset += 1
+            Left(c)
+        }
+      } else {
+        isPart = true
+        val argTree = argsIter.next()
+        if (partsIter.hasNext) {
+          val tree = partsIter.next()
+          val Literal(Constant(s: String)) = tree
+          current = Some(s)
+          start = tree.pos.startOrPoint
+          offset = 0
+        } else {
+          current = None
+          start = argTree.pos.endOrPoint
+          offset = 0
+        }
+        Right(argTree)
+      }
+    }
+  }
+
 }
