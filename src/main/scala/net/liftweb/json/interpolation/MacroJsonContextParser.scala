d5273c422064ada0ed698e569a579498fad106e3
iron9light <iron9light@gmail.com>
2012-11-26 03:10:50 +0800

    WIP: implementing MacroJsonContextParser.

--- src/main/scala/net/liftweb/json/interpolation/MacroJsonContextParser.scala
+++ src/main/scala/net/liftweb/json/interpolation/MacroJsonContextParser.scala
@@ -19,17 +19,113 @@ package interpolation
 
 import reflect.macros.Context
 import annotation.tailrec
+import java.util
 
 abstract class MacroJsonContextParser {
   val context: Context
 
   import context.universe._
 
+  sealed abstract class Token {
+    var pos: Position = NoPosition
+  }
+
+  case object OpenObj extends Token
+
+  case object CloseObj extends Token
+
+  case object OpenFieldName extends Token
+
+  case object CloseFieldName extends Token
+
+  case object OpenString extends Token
+
+  case object CloseString extends Token
+
+  case object End extends Token
+
+  case class StringVal(value: String) extends Token
+
+  case class IntVal(value: BigInt) extends Token
+
+  case class DoubleVal(value: Double) extends Token
+
+  case class BoolVal(value: Boolean) extends Token
+
+  case class ObjVal(value: Tree) extends Token {
+    pos = value.pos
+  }
+
+  case object NullVal extends Token
+
+  case object OpenArr extends Token
+
+  case object CloseArr extends Token
+
   private val EOF = (-1).asInstanceOf[Char]
 
   def parse(parts: Seq[Tree], args: Seq[Tree]): context.Expr[JValue] = {
     // todo
     context.literalNull
+  }
+
+  private[interpolation] class Parser(buf: Buffer) {
+    private[this] val blocks = new java.util.LinkedList[BlockMode]()
+    private[this] val fieldNameMode = true
+    private[this] val stringMode = false
+
+    private[this] val tokenQueen = new java.util.LinkedList[Token]()
+
+    def nextToken: Token = {
+      def isDelimiter(c: Char) = c == ' ' || c == '\n' || c == ',' || c == '\r' || c == '\t' || c == '}' || c == ']'
+
+      def parseString: String = ???
+
+      def parseValue(first: Char) = {
+        var wasInt = true
+        var doubleVal = false
+        val s = new StringBuilder
+        s.append(first)
+        while (wasInt) {
+          val Left(c) = buf.next
+          if (c == '.' || c == 'e' || c == 'E') {
+            doubleVal = true
+            s.append(c)
+          } else if (!(Character.isDigit(c) || c == '.' || c == 'e' || c == 'E' || c == '-')) {
+            wasInt = false
+            buf.back()
+          } else s.append(c)
+        }
+        val value = s.toString()
+        if (doubleVal) DoubleVal(parseDouble(value))
+        else IntVal(BigInt(value))
+      }
+
+      while (true) {
+        if (!tokenQueen.isEmpty) {
+          return tokenQueen.poll()
+        }
+        // todo
+      }
+
+      End
+    }
+
+    sealed abstract class BlockMode
+
+    case object ArrayMode extends BlockMode
+    case object ObjectMode extends BlockMode
+    case object StringMode extends BlockMode
+  }
+
+  // FIXME fail fast to prevent infinite loop, see
+  // http://www.exploringbinary.com/java-hangs-when-converting-2-2250738585072012e-308/
+  private val BrokenDouble = BigDecimal("2.2250738585072012e-308")
+
+  private[interpolation] def parseDouble(s: String) = {
+    val d = BigDecimal(s)
+    if (d == BrokenDouble) sys.error("Error parsing 2.2250738585072012e-308")
+    else d.doubleValue
   }
 
   private[interpolation] class Buffer(parts: Seq[Tree], args: Seq[Tree]) {
@@ -53,6 +149,10 @@ abstract class MacroJsonContextParser {
     var eofIsFailure = false
 
     def point: Int = start + offset
+
+    def back() {
+      offset -= 1
+    }
 
     @tailrec
     final def next: Either[Char, Tree] = {
